2d5d60f7fa1da7f5bdf10ff7b4144ee989607a0b
Darshan Bhatta <bhatta.darshan26@gmail.com>
2020-01-11 21:21:53 -0800

    War Card game

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    @IBOutlet weak var dealBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func dealClick(_ sender: Any) {
+        let leftCardIndex = Int.random(in: 2...14)
+        let rightCardIndex = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftCardIndex)")
+        
+        rightImageView.image = UIImage(named: "card\(rightCardIndex)")
+        
+        if leftCardIndex > rightCardIndex {
+            leftScoreLabel.text = String( Int(leftScoreLabel.text!)! + 1)
+        } else if leftCardIndex < rightCardIndex {
+            rightScoreLabel.text = String( Int(rightScoreLabel.text!)! + 1)
+        }
+        
+    }
+    
 }
 

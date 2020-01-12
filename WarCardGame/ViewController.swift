5d7c26ac75427bbba2ee7e74804ad01c3f8d19b5
Darshan Bhatta <bhatta.darshan26@gmail.com>
2020-01-11 18:49:50 -0800

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Darshan on 1/11/20.
+//  Copyright © 2020 Darshan Bhatta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
